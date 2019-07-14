800fd45f05476ff0f88084f34d11587b19eb36fe
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-07-13 21:33:18 -0700

    Mixed currency tests passing

--- src/Money.ts
+++ src/Money.ts
@@ -1,95 +1,103 @@
 import HashTable from './HashTable'
 
 export interface Expression {
-	reduce(bank: Bank, to: string): Money
+  reduce(bank: Bank, to: string): Money
+  plus(addend: Expression): Expression
 }
 
 class Money implements Expression {
-	static dollar = (amount: number): Money => {
-		return new Money(amount, "USD")
-	}
+  static dollar = (amount: number): Money => {
+    return new Money(amount, 'USD')
+  }
 
-	static franc = (amount: number): Money => {
-		return new Money(amount, "CHF")
-	}
+  static franc = (amount: number): Money => {
+    return new Money(amount, 'CHF')
+  }
 
-	constructor(public amount: number, public currency: string) {
-		this.amount = amount
-		this.currency = currency
-	}
+  constructor(public amount: number, public currency: string) {
+    this.amount = amount
+    this.currency = currency
+  }
 
-	public equals = (money: Money): boolean => {
-		const { amount, currency }: Money = money
-		const isEqualAmt = this.amount == amount
-		const isEqualCurrency = this.currency == currency
+  public equals = (money: Money): boolean => {
+    const { amount, currency }: Money = money
+    const isEqualAmt = this.amount == amount
+    const isEqualCurrency = this.currency == currency
 
-		return isEqualAmt && isEqualCurrency
-	}
+    return isEqualAmt && isEqualCurrency
+  }
 
-	public times = (multiplier: number): Money => {
-		return new Money(this.amount * multiplier, this.currency)
-	}
+  public times = (multiplier: number): Money => {
+    return new Money(this.amount * multiplier, this.currency)
+  }
 
-	public plus = (addend: Money): Sum => {
-		return new Sum(this, addend)
-	}
+  public plus = (addend: Expression): Expression => {
+    return new Sum(this, addend)
+  }
 
-	public reduce = (bank: Bank, to: string): Money => {
-		const rate: number = bank.rate(this.currency, to)
-		return new Money(this.amount / rate, to)
-	}
+  public reduce = (bank: Bank, to: string): Money => {
+    const rate: number = bank.rate(this.currency, to)
+    return new Money(this.amount / rate, to)
+  }
 }
 
 // BANK
 export class Bank {
-	reduce = (source: Expression, to: string): Money => {
-		return source.reduce(this, to)
-	}
+  public reduce = (source: Expression, to: string): Money => {
+    return source.reduce(this, to)
+  }
 
-	public rate = (from: string, to: string): number => {
-		if (from == to) {
-			return 1
-		}
+  public rate = (from: string, to: string): number => {
+    if (from == to) {
+      return 1
+    }
 
-		const rate: number = this.rates.get(new Pair(from, to)) as number
-		return rate
-	}
+    const rate: number = this.rates.get(new Pair(from, to)) as number
+    return rate
+  }
 
-	public addRate = (from: string, to: string, rate: number) => {
-		this.rates.set(new Pair(from, to), rate)
-	}
+  public addRate = (from: string, to: string, rate: number) => {
+    this.rates.set(new Pair(from, to), rate)
+  }
 
-	public rates: HashTable<number> = new HashTable()
+  public rates: HashTable<number> = new HashTable()
 }
 
 // SUM
 export class Sum implements Expression {
-	constructor(
-		public augend: Money,
-		public addend: Money
-	) { }
+  constructor(
+    public augend: Expression,
+    public addend: Expression
+  ) { }
 
-	reduce = (bank: Bank, to: string): Money => {
-		const amount = this.augend.amount + this.addend.amount
-		return new Money(amount, to)
-	}
+  public reduce = (bank: Bank, to: string): Money => {
+    const amount =
+      this.augend.reduce(bank, to).amount
+      + this.addend.reduce(bank, to).amount
+
+    return new Money(amount, to)
+  }
+
+  public plus = (addend: Expression): Expression => {
+    return this
+  }
 }
 
 // PAIR
 export class Pair {
-	constructor(private from: string, private to: string) {
-		this.from = from
-		this.to = to
-	}
+  constructor(private from: string, private to: string) {
+    this.from = from
+    this.to = to
+  }
 
-	public equals(object: Object) {
-		const pair: Pair = object as Pair
-		return this.from == pair.from && this.to == pair.to
-	}
+  public equals(object: Object) {
+    const pair: Pair = object as Pair
+    return this.from == pair.from && this.to == pair.to
+  }
 
-	public hashCode = () => {
-		return 0
-	}
+  public hashCode = () => {
+    return 0
+  }
 }
 
 
--- src/__tests__/Money.test.ts
+++ src/__tests__/Money.test.ts
@@ -1,91 +1,93 @@
 import Money, { Bank, Expression, Sum } from '../Money'
-import HashTable, { Hashable } from '../HashTable'
 
 describe('Money', () => {
 
-	test('Multiplication', () => {
-		const five: Money = Money.dollar(5)
-		expect(Money.dollar(10).equals(five.times(2))).toBe(true)
-		expect(Money.dollar(15).equals(five.times(3))).toBe(true)
-	})
+  test('Multiplication', () => {
+    const five: Money = Money.dollar(5)
+    expect(Money.dollar(10).equals(five.times(2))).toBe(true)
+    expect(Money.dollar(15).equals(five.times(3))).toBe(true)
+  })
 
-	test('Equality', () => {
-		expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true)
-		expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false)
-		expect(Money.franc(5).equals(Money.dollar(5))).toBe(false)
-	})
+  test('Equality', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true)
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false)
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false)
+  })
 
-	test('Currency descriptors', () => {
-		expect('USD').toEqual(Money.dollar(1).currency)
-		expect('CHF').toEqual(Money.franc(1).currency)
-	})
+  test('Currency descriptors', () => {
+    expect('USD').toEqual(Money.dollar(1).currency)
+    expect('CHF').toEqual(Money.franc(1).currency)
+  })
 
-	test('Addition', () => {
-		const five: Money = Money.dollar(5)
-		const sum: Expression = five.plus(five)
-		const bank: Bank = new Bank()
-		const reduced: Money = bank.reduce(sum, 'USD')
-		expect(Money.dollar(10).equals(reduced)).toBe(true)
-	})
+  test('Addition', () => {
+    const five: Money = Money.dollar(5)
+    const sum: Expression = five.plus(five)
+    const bank: Bank = new Bank()
+    const reduced: Money = bank.reduce(sum, 'USD')
+    expect(Money.dollar(10).equals(reduced)).toBe(true)
+  })
 
-	test('Plus returns Sum', () => {
-		const five: Money = Money.dollar(5)
-		const result: Expression = five.plus(five)
-		const sum: Sum = result as Sum
-		expect(five).toEqual(sum.augend)
-		expect(five).toEqual(sum.addend)
-	})
+  test('Plus returns Sum', () => {
+    const five: Money = Money.dollar(5)
+    const result: Expression = five.plus(five)
+    const sum: Sum = result as Sum
+    expect(five).toEqual(sum.augend)
+    expect(five).toEqual(sum.addend)
+  })
 
-	test('Reduce Sum', () => {
-		const sum: Expression = new Sum(
-			Money.dollar(3),
-			Money.dollar(4)
-		)
-		const bank: Bank = new Bank()
-		const result: Money = bank.reduce(sum, 'USD')
+  test('Reduce Sum', () => {
+    const sum: Expression = new Sum(
+      Money.dollar(3),
+      Money.dollar(4)
+    )
+    const bank: Bank = new Bank()
+    const result: Money = bank.reduce(sum, 'USD')
 
-		expect(Money.dollar(7).equals(result)).toBe(true)
-	})
+    expect(Money.dollar(7).equals(result)).toBe(true)
+  })
 
-	test('Reduce Money', () => {
-		const bank: Bank = new Bank()
-		const result: Money = bank.reduce(Money.dollar(1), 'USD')
-		expect(Money.dollar(1).equals(result)).toBe(true)
-	})
+  test('Reduce Money', () => {
+    const bank: Bank = new Bank()
+    const result: Money = bank.reduce(Money.dollar(1), 'USD')
+    expect(Money.dollar(1).equals(result)).toBe(true)
+  })
 
-	test('Reduce Money, Different Currency', () => {
-		const bank: Bank = new Bank()
-		bank.addRate('CHF', 'USD', 2)
-		const result: Money = bank.reduce(Money.franc(2), 'USD')
-		expect(Money.dollar(1).equals(result)).toBe(true)
-	})
+  test('Reduce Money, Different Currency', () => {
+    const bank: Bank = new Bank()
+    bank.addRate('CHF', 'USD', 2)
+    const result: Money = bank.reduce(Money.franc(2), 'USD')
+    expect(Money.dollar(1).equals(result)).toBe(true)
+  })
 
-	test('Identity Rate', () => {
-		expect(new Bank().rate('USD', 'USD')).toEqual(1)
-	})
+  test('Identity Rate', () => {
+    expect(new Bank().rate('USD', 'USD')).toEqual(1)
+  })
+
+  test('Mixed Addition', () => {
+    const fiveBucks: Expression = Money.dollar(5)
+    const tenFrancs: Expression = Money.franc(10)
+    const bank: Bank = new Bank()
+    bank.addRate('CHF', 'USD', 2)
+    const result: Money = bank.reduce(fiveBucks.plus(tenFrancs), 'USD')
+    expect(Money.dollar(10).equals(result)).toBe(true)
+  })
 
 })
 
-test('HashTable', () => {
-	const hashTable: HashTable<number> = new HashTable()
-	const hashable: Hashable = {
-		hashCode: () => 0
-	}
 
-	hashTable.set(hashable, 2)
-	expect(hashTable.get(hashable)).toEqual(2)
-})
 
 /**
  * TODO
  *
- * [] $5 + 10 CHF = $10 if rate is 2:1
- * [] $5 + $5 = $10
+ * [X] $5 + 10 CHF = $10 if rate is 2:1
+ * [X] $5 + $5 = $10
  * [] Return Money from $5 + $5
  * [X] Bank.reduce(Money)
  * [X] Reduce Money with conversion
  * [X] Reduce(Bank, String)
  * [X] HashTable
+ * [] Sum.plus
+ * [] Expression.times
  * [X] $5 * 2 = $10
  * [X] Make `amount` private
  * [X] Dollar side effects?
